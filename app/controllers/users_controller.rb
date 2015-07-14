78347c733e6f8c59b1e321f2d10823814487ea6b
Charles Zahn <charlesazahn@gmail.com>
2015-07-14 08:47:31 -0400

    Add user controller and new user view.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render text: "Successfully created account!"
+    else
+      render @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
+end
