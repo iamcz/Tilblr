dba651fe8449c6590802e5850df63bfb6dbd8192
Charles Zahn <charlesazahn@gmail.com>
2015-07-14 08:00:23 -0400

    Add basic user validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  attr_accessor :password
+  validates :username, presence: true
+  validates :password, length: {minimum: 8, allow_nil: true}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  def password=(password) 
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
