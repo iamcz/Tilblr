e704ebb262d42088b43356c78670be54e982b703
Charles Zahn <charlesazahn@gmail.com>
2015-07-14 11:53:51 -0400

    Add styling for login form.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,4 +27,11 @@ RSpec.describe User, type: :model do
 
     expect(User.find_by_credentials(user.email, user.password)).to eq(user)
   end
+
+  it "doesn't store the password" do
+    user = build(:user) 
+    expect(user.password).not_to eq(user.password_digest)
+
+    expect(User.column_names).not_to include("password")
+  end
 end
