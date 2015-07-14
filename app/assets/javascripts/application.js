1fa4934db9bd386dfdf6106e116533d040a93c26
Charles Zahn <charlesazahn@gmail.com>
2015-07-14 17:22:45 -0400

    remove collecitons from application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,6 @@
 //= require tilblr
 //= require_tree ../templates
 //= require_tree ./models
-//= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
