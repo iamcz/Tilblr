69b4b229953c7a25c2efc5f6b992bbff357ecb36
Charles Zahn <charlesazahn@gmail.com>
2015-07-15 14:28:33 -0400

    Add post show backbone view.

---
+++ app/assets/javascripts/views/posts/show.js
@@ -0,0 +1,15 @@
+Tilblr.Views.PostShow = Backbone.View.extend({
+  template: JST["posts/show"],
+  tagName: "article",
+  className: "post",
+
+  initialize: function () {
+    this.listenTo(this.model, "sync", this.render);
+  },
+
+  render: function () {
+    this.$el.html(this.template({post: this.model}));
+
+    return this;
+  }
+})
