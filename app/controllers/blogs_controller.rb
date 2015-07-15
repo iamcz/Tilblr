2bc0adf700a1c84b03b943362f6704f620375d4b
Charles Zahn <charlesazahn@gmail.com>
2015-07-15 06:59:32 -0400

    Added create action in blog controller and associated route

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,17 @@
 class BlogsController < ApplicationController
   def show
-    @blog = Blog.find(params[:id]);
+    @blog = Blog.find(params[:id])
+  end
+
+  def create
+    @blog = current_user.blogs.new(blog_params)
+
+    if @blog.save
+      current_user.active_blog = @blog
+    else
+      flash[:errors] = @blog.errors.full_messages
+    end
+
+    redirect_to current_user.active_blog
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
 
-  resources :blogs, only: [:show]
+  resources :blogs, only: [:show, :create]
 
   namespace :api, defaults: { format: :json } do
     resources :blogs, only: [:show]
