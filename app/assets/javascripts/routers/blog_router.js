cd8bc718c9d13cac3c4c42e82ccc882c8820c8d5
Charles Zahn <charlesazahn@gmail.com>
2015-07-14 22:40:23 -0400

    Add container composite view.

--- app/assets/javascripts/routers/blog_router.js
+++ app/assets/javascripts/routers/blog_router.js
@@ -1,16 +1,31 @@
 Tilblr.Routers.BlogRouter = Backbone.Router.extend({
   routes: {
-    "": "index"
+    "": "postIndex"
   },
 
   initialize: function (options) {
     this.model = options.model;
+    this.collection = options.collection;
     this.$rootEl = options.$rootEl;
+
+    this.setupViews();
   },
 
-  index: function () {
+  setupViews: function () {
+    this._containerView = new Tilblr.Views.ContainerView({
+      $el: this.$rootEl
+    });
+
+    // this._profileView = new Tilblr.Views.ProfileView({
+    //   collection: this.collection
+    // });
+
+    // this._containerView.addSubview("#profile-button", this._profileView);
+  },
+
+  postIndex: function () {
     this.model.fetch();
     var blogView = new Tilblr.Views.BlogShow({model: this.model})
-    this.$rootEl.html(blogView.render().$el);
+    // this._containerView.html(blogView.render().$el);
   }
 });
---
+++ app/assets/javascripts/views/container.js
@@ -0,0 +1,23 @@
+Tilblr.Views.ContainerView = Backbone.CompositeView.extend({
+  events: {
+    "click": "removeProfileDropdown",
+    "click #profile-button": "addProfileDropdown"
+  },
+
+  initialize: function () {
+    this._profileView = new Tilblr.Views.ProfileView({
+      model: this.model,
+      collection: this.collection
+    });
+  },
+
+  profileDropdown: function (event) {
+    event.stopPropegation();
+
+    this.addSubview("#profile", this._profileView)
+  },
+
+  removeProfileDropdown: function (event) {
+    this.removeSubview("#profile");
+  }
+})
