99696a7fb923bad366f9c05b6dc76491672d8056
Charles Zahn <charlesazahn@gmail.com>
2015-07-15 11:54:18 -0400

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  name = Faker::Name.name
+  email = Faker::Internet.email(name)
+  username = Faker::Internet.user_name(name)
+  password = "password"
+
+  user = User.new(email: email, username: username, password: password)
+  user.save
+  5.times do |i|
+    title = "blog-#{i}"
+    blog = user.blogs.new(title: title)
+    blog.save
+
+    10.times do |j|
+      title = "post-#{j}"
+      body = Faker::Lorem.paragraphs(1)
+      post = blog.posts.new(title: title, body: body)
+      post.save
+    end
+  end
+end
