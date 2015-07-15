0fe3f8ef4366367fe650658d3a833bc78431684d
Charles Zahn <charlesazahn@gmail.com>
2015-07-15 10:49:27 -0400

    Add post factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,16 @@
 FactoryGirl.define do
   factory :post do
     title "MyString"
-body "MyText"
-blog nil
+    body "MyText"
+    blog_id 0
+
+    factory :post_with_blog_and_user do
+      after(:build) do |post|
+        user = build(:user) 
+        blog = build(:blog, user: user)
+        post.blog = blog
+      end
+    end
   end
 
 end
