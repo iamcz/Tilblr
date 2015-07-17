8f9ef542d79014f1b375766a41dbbf9ff84c1e3a
Charles Zahn <charlesazahn@gmail.com>
2015-07-17 10:09:11 -0400

    Add follows#findOrFetch method to backbone collection.

--- app/assets/javascripts/collections/follows.js
+++ app/assets/javascripts/collections/follows.js
@@ -1,3 +1,14 @@
 Tilblr.Collections.Follows = Backbone.Collection.extend({
-  model: Tilblr.Models.Follow
+  model: Tilblr.Models.Follow,
+
+  findOrFetch: function (attributes) {
+    var follow = this.findWhere(attributes);
+
+    if (typeof follow === "undefined") {
+      follow = new Tilblr.Models.Follow(attributes);
+      follow.fetch();
+    }
+
+    return follow;
+  }
 });
