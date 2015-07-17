6e9aa25a0494243dde32fa5d257ec2cf9e13927b
Charles Zahn <charlesazahn@gmail.com>
2015-07-17 11:25:43 -0400

    fix bug in follow button and clear model on destroy.

--- app/assets/javascripts/views/follows/button.js
+++ app/assets/javascripts/views/follows/button.js
@@ -6,10 +6,10 @@ Tilblr.Views.FollowButton = Backbone.View.extend({
   },
 
   initialize: function () {
-    this.listenTo(this.model, "sync", this.render);
+    this.listenTo(this.model, "sync change", this.render);
   },
 
-  this.render: function () {
+  render: function () {
     var text;
     if (this.model.isNew()) {
       text = "Follow";
@@ -19,21 +19,20 @@ Tilblr.Views.FollowButton = Backbone.View.extend({
 
     this.$el.prop({disabled: false});
     this.$el.html(text);
-  }
+  },
 
   toggleFollow: function () {
     this.$el.prop({disabled: true});
 
     if (this.model.isNew()) {
-      this.model.save();
+      this.model.save({}, {wait: true});
     } else {
       this.model.destroy({
+        wait: true;
         success: function () {
-          var followed_id = this.model.get("followed_id")
-          // this.collection.remove(this.model);
-          this.model = new Tilblr.Models.Follow({});
-          this.model.fetch();
-          // this.collection.add(this.model);
+          var followed_id = this.model.get("followed_id");
+          this.model.clear();
+          this.model.set({followed_id: followed_id})
         }.bind(this)
       });
     }
