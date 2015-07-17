d2246be237a697183c87d71f4e8ec2411fbf0375
Charles Zahn <charlesazahn@gmail.com>
2015-07-17 08:45:19 -0400

    Fixed mistake in assigning follows to users.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,10 +5,10 @@ class Blog < ActiveRecord::Base
   has_one :active_user, class_name: "User", foreign_key: :active_blog_id, inverse_of: :active_blog
 
   has_many :posts, inverse_of: :blog
-  has_many :follows_follows, class_name: "Follow", foreign_key: :follower_id, inverse_of: :followee
-  has_many :follows, through: :follows_follows, source: :followee
-  has_many :followers_follows, class_name: "Follow", foreign_key: :followee_id, inverse_of: :followee
-  has_many :followers, through: :followers_follows, source: :follower
+  has_many :followed_follows, class_name: "Follow", foreign_key: :follower_id, inverse_of: :follower
+  has_many :followed_blogs, through: :followed_follows, source: :followed
+  has_many :follower_follows, class_name: "Follow", foreign_key: :followed_id, inverse_of: :followed
+  has_many :follower_blogs, through: :follower_follows, source: :follower
 
   def follow(blog) 
     follow = Follow.new(follower_id: self.id, followee_id: blog.id)
