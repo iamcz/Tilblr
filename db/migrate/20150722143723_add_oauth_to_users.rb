ce0b21ed2fc4ca60549583eafa0b6209486bc7b5
Charles Zahn <charlesazahn@gmail.com>
2015-07-22 10:42:02 -0400

    Add oauth migration for users.

---
+++ db/migrate/20150722143723_add_oauth_to_users.rb
@@ -0,0 +1,8 @@
+class AddOauthToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+
+    add_index :users, [:provider, :uid], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720143831) do
+ActiveRecord::Schema.define(version: 20150722143723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,8 +73,11 @@ ActiveRecord::Schema.define(version: 20150720143831) do
     t.datetime "updated_at",      null: false
     t.string   "session_token",   null: false
     t.integer  "active_blog_id"
+    t.string   "provider"
+    t.string   "uid"
   end
 
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "blogs", "users"
