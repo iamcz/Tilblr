1b62703c1397406b46988a052caf13e990c7e544
Charles Zahn <charlesazahn@gmail.com>
2015-07-23 13:44:02 -0400

    add a current user api route and action.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,4 +9,20 @@ class Api::UsersController < ApplicationController
         ]
       ).find(params[:id])
   end
+
+  def current_user
+    @user = User
+      .includes(
+        :blogs, 
+        :active_blog => [ 
+          :followed_follows, 
+          :follower_follows 
+        ]
+      ).find_by(session_token: session[:session_token])
+    if @user
+      render :show
+    else
+      render json: nil
+    end
+  end
 end
