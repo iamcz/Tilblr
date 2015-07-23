3384a6b9c695a5702f65205f9be6d4d5f2e38432
Charles Zahn <charlesazahn@gmail.com>
2015-07-23 17:18:02 -0400

    Add helper functions to post show view.

--- app/assets/javascripts/views/posts/show.js
+++ app/assets/javascripts/views/posts/show.js
@@ -11,5 +11,20 @@ Tilblr.Views.PostShow = Backbone.CompositeView.extend({
     this.$el.html(this.template({post: this.model}));
 
     return this;
+  },
+
+  addHeader: function () {
+    var postHeader = new Tilblr.Views.PostHeader({model: this.model});
+    this.addSubview(".post-header", postHeader);
+  },
+
+  addFooter: function () {
+    var postFooter = new Tilblr.Views.PostFooter({model: this.model});
+    this.addSubview(".post-footer", postFooter);
+  },
+
+  addBlogAvatar: function () {
+    var blogAvatar = new Tilblr.Views.BlogAvatar({model: this.model.blog()});
+    this.addSubview(".blog-avatar", blogAvatar);
   }
 })
