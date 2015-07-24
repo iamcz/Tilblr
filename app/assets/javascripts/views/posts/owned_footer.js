d54c23cfa90490aa630d7e1c54589fe5917b2034
Charles Zahn <charlesazahn@gmail.com>
2015-07-23 22:05:51 -0400

    add styles + rename file.

--- app/assets/javascripts/views/posts/owned_footer.js
+++ app/assets/javascripts/views/posts/owned_footer.js
@@ -1,6 +1,7 @@
 Tilblr.Views.OwnedFooter = Backbone.CompositeView.extend({
   template: JST["posts/owned_footer"],
   tagName: "section",
+  className: "group",
 
   render: function () {
     this.$el.html(this.template({model: this.model}));
--- app/assets/javascripts/views/posts/unowned_footer.js
+++ app/assets/javascripts/views/posts/unowned_footer.js
@@ -1,6 +1,7 @@
 Tilblr.Views.UnownedFooter = Backbone.View.extend({
   template: JST["posts/unowned_footer"],
   tagName: "section",
+  className: "group",
 
   render: function () {
     this.$el.html(this.template());
