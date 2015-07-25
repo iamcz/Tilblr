7f85ba56a93db17d7f0268ca77d212ac4a53a10c
Charles Zahn <charlesazahn@gmail.com>
2015-07-25 11:27:19 -0400

    change some styles add form authentication helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def form_authentication
+    <<-HTML 
+      <input 
+        type="hidden" 
+        name="authenticity_token" 
+        value="<%= form_authenticity_token %>">
+    HTML
+  end
 end
